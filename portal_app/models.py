26a282e7ab1a18b8ae65ee217f5f859559833f85
Nick Gonzalez <ngonzo95@gmail.com>
2023-08-07 21:16:27 -0700

    fixed Decimals printing.

--- portal_app/models.py
+++ portal_app/models.py
@@ -1,34 +1,35 @@
 from django.db import models
+from decimal import Decimal
 
 # Create your models here.
 class Intersection(models.Model):
-    lat = models.DecimalField(db_index=True, max_digits=20, decimal_places=16)
-    lon = models.DecimalField(db_index=True, max_digits=20, decimal_places=16)
-    intersection_type = models.CharField(db_index=True, max_length=80) 
-    number_of_accidents = models.IntegerField(db_index=True)
+    lat: Decimal = models.DecimalField(db_index=True, max_digits=20, decimal_places=16)
+    lon: Decimal = models.DecimalField(db_index=True, max_digits=20, decimal_places=16)
+    intersection_type: str = models.CharField(db_index=True, max_length=80) 
+    number_of_accidents: int = models.IntegerField(db_index=True)
     fatal_accident_number = models.IntegerField(db_index=True)
-    major_accident_number = models.IntegerField(db_index=True) 
-    minor_accident_number = models.IntegerField()  
-    unknown_accident_number = models.IntegerField() 
-    property_accident_number = models.IntegerField()
-    average_cost_to_insurers = models.IntegerField()
+    major_accident_number: int = models.IntegerField(db_index=True) 
+    minor_accident_number: int = models.IntegerField()  
+    unknown_accident_number: int = models.IntegerField() 
+    property_accident_number: int = models.IntegerField()
+    average_cost_to_insurers: int = models.IntegerField()
 
     @property
     def intervention_count(self):
         return self.intervention_set.all().count()
 
     @property
-    def formatted_cost_to_insurers(self):
+    def formatted_cost_to_insurers(self): 
         if type(self.average_cost_to_insurers) == str:
-            return self.average_cost_to_insurers
+            return '${:0,.2f}'.format(Decimal(self.average_cost_to_insurers))
         else:
             return  '${:0,.2f}'.format(self.average_cost_to_insurers)
 
 class Intervention(models.Model):
-    new_intersection_type = models.CharField(db_index=True, max_length=80)
-    conversion_cost = models.DecimalField(db_index=True, max_digits=20, decimal_places=2) 
-    accident_reduction_rate = models.DecimalField(max_digits=10, decimal_places=8) 
-    accident_severity_reduction_rate = models.DecimalField(max_digits=10, decimal_places=8) 
+    new_intersection_type: str = models.CharField(db_index=True, max_length=80)
+    conversion_cost: Decimal  = models.DecimalField(db_index=True, max_digits=20, decimal_places=2) 
+    accident_reduction_rate: Decimal  = models.DecimalField(max_digits=10, decimal_places=8) 
+    accident_severity_reduction_rate: Decimal  = models.DecimalField(max_digits=10, decimal_places=8) 
     intersection = models.ForeignKey(
         "Intersection",
         on_delete=models.CASCADE,
@@ -37,6 +38,6 @@ class Intervention(models.Model):
     @property
     def formatted_conversion_cost(self):
         if type(self.conversion_cost) == str:
-            return self.conversion_cost
+            return '${:0,.2f}'.format(Decimal(self.conversion_cost))
         else:
             return  '${:0,.2f}'.format(self.conversion_cost)
